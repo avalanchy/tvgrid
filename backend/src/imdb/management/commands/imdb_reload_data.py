6db29275ebdfa6d8c59d0d1368cb91d58f8bd7c9
Mariusz Osiecki <avalanchy@gmail.com>
2020-06-27 14:05:03 +0200

    Tweak command logs

--- backend/src/imdb/management/commands/imdb_reload_data.py
+++ backend/src/imdb/management/commands/imdb_reload_data.py
@@ -12,6 +12,12 @@ from ...models import Title, Episode, Rating
 class Command(BaseCommand):
     help = "Load data from IMDb Datasets"
 
+    MODELS_AND_URLS = (
+        (Title, "https://datasets.imdbws.com/title.basics.tsv.gz"),
+        (Episode, "https://datasets.imdbws.com/title.episode.tsv.gz"),
+        (Rating, "https://datasets.imdbws.com/title.ratings.tsv.gz"),
+    )
+
     @staticmethod
     def download_file(url):
         """Function downloads large files in with requests with low memory
@@ -19,14 +25,14 @@ class Command(BaseCommand):
 
         Based on https://stackoverflow.com/a/16696317/1765615.
         """
-        filename = url.split('/')[-1]
+        filename = url.split("/")[-1]
         local_filepath = f"/tmp/{filename}"
         if os.path.exists(local_filepath):
             # file already exists
             return local_filepath
         with requests.get(url, stream=True) as r:
             r.raise_for_status()
-            with open(local_filepath, 'wb') as f:
+            with open(local_filepath, "wb") as f:
                 for chunk in r.iter_content(chunk_size=8192):
                     if chunk:
                         # filter out keep-alive new chunks
@@ -43,23 +49,21 @@ class Command(BaseCommand):
                 file=f,
                 table=model._meta.db_table,
                 sep="\t",
-                columns=[f.attname for f in model._meta.concrete_fields]
+                columns=[f.attname for f in model._meta.concrete_fields],
             )
 
     def handle(self, *args, **options):
-        for url, model in (
-            ("https://datasets.imdbws.com/title.basics.tsv.gz", Title),
-            ("https://datasets.imdbws.com/title.episode.tsv.gz", Episode),
-            ("https://datasets.imdbws.com/title.ratings.tsv.gz", Rating),
-        ):
-            self.stdout.write(f"{model.__name__}: Downloading data dump {url}.")
+        for i, (model, url) in enumerate(self.MODELS_AND_URLS, start=1):
+            log_prefix = f"{i}/{len(self.MODELS_AND_URLS)} {model.__name__}\t"
+
+            self.stdout.write(f"{log_prefix} Downloading data dump {url}.")
             filepath = self.download_file(url)
 
             with atomic():
-                self.stdout.write(f"{model.__name__}: Deleting existing data.")
+                self.stdout.write(f"{log_prefix} Deleting existing data.")
                 model.objects.all().delete()
 
-                self.stdout.write(f"{model.__name__}: Loading data from {filepath}.")
+                self.stdout.write(f"{log_prefix} Loading data from {filepath}.")
                 self.copy_to_table(filepath, model)
 
         self.stdout.write("Done.")
